d27f59ebe40af991963010deec471889d71aa777
awaesome <yevhenii.korobchuk@vgs.io>
2021-08-13 18:13:36 +0300

    added collect form and sync it w/ netlify submissions

---
+++ collect-form.js
@@ -0,0 +1,28 @@
+const vgsForm = window.VGSCollect.create('YOUR_VAULT_TENANT', 'SANDBOX', () => {});
+
+const css = {
+  boxSizing: 'border-box',
+  fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI"',
+  color: '#40545F',
+};
+
+vgsForm.field('#cc-holder', {
+  type: 'text',
+  name: 'name',
+  placeholder: 'Card holder',
+  validations: ['required'],
+  css: css,
+});
+
+vgsForm.field('#cc-number', {
+  type: 'card-number',
+  name: 'number',
+  successColor: '#4F8A10',
+  errorColor: '#D8000C',
+  placeholder: 'Card number',
+  showCardIcon: true,
+  validations: ['required', 'validCardNumber'],
+  css: css,
+});
+
+export default vgsForm;
---
+++ index.js
@@ -0,0 +1,64 @@
+import vgsForm from './collect-form.js'
+
+function transformExpDate() {
+  this.type = 'text';
+  let input = this.value;
+  if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3);
+  const expireDate = input.split('/').map(function(v) {
+    return v.replace(/\D/g, '');
+  });
+  const output = expireDate.map(function(v, i) {
+    return v.length == 2 && i < 2 ? v + ' / ' : v;
+  });
+  this.value = output.join('').substr(0, 7);
+};
+
+const input = document.querySelector('#expiration-date');
+input.addEventListener('input', transformExpDate.bind(input));
+
+// input validation to avoid typing non-number symbols
+input.addEventListener('keypress', e => {
+  if(e.code.includes('Key')) {
+    e.preventDefault();
+  }
+})
+
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
+const handleSubmit = (data) => {
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: encode({
+      "form-name": "netlify-form",
+      ...data,
+    })
+  }).catch(error => alert(error))
+}
+
+document.getElementById("collect-form").addEventListener("submit", async(e) => {
+  e.preventDefault();
+  const fields = document.querySelectorAll("input.form-field");
+  vgsForm.submit(
+    "/post",
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    },
+    (status, { json }) => {
+      fields.forEach(field => {
+        json[field.name || field.id] = field.value
+      })
+      handleSubmit(json)
+    },
+    function(errors) {
+      console.error(errors);
+    }
+  );
+});
