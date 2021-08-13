cb2847801892fc2eea7263b74b45e5c36d58cb62
awaesome <yevhenii.korobchuk@vgs.io>
2021-08-13 23:56:56 +0300

    collect js and yaml

--- index.js
+++ index.js
@@ -1,61 +1,36 @@
-import vgsForm from './collect-form.js'
+// get YOUR_VAULT_TENANT in the VGS Dashboard -> Home -> under the page header -> Vault ID
+const vgsForm = window.VGSCollect.create('YOUR_VAULT_TENANT', 'SANDBOX', () => {});
 
-function transformExpDate() {
-  this.type = 'text';
-  let input = this.value;
-  if (/\D\/$/.test(input)) input = input.substr(0, input.length - 3);
-  const expireDate = input.split('/').map(function(v) {
-    return v.replace(/\D/g, '');
-  });
-  const output = expireDate.map(function(v, i) {
-    return v.length == 2 && i < 2 ? v + ' / ' : v;
-  });
-  this.value = output.join('').substr(0, 7);
+const css = {
+  boxSizing: 'border-box',
+  fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI"',
+  color: '#40545F',
+  fontSize: '1rem',
 };
 
-const input = document.querySelector('#expiration-date');
-input.addEventListener('input', transformExpDate.bind(input));
-
-// input validation to avoid typing non-number symbols
-input.addEventListener('keypress', e => {
-  if(e.code.includes('Key')) {
-    e.preventDefault();
-  }
-})
-
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&")
-}
-
-const handleSubmit = (data) => {
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: encode({
-      "form-name": "netlify-form",
-      ...data,
-    })
-  }).catch(error => alert(error))
-}
+vgsForm.field('#secure-field', {
+  type: 'text',
+  name: 'secure-field',
+  css: css,
+});
 
 document.getElementById("collect-form").addEventListener("submit", async(e) => {
   e.preventDefault();
-  const fields = document.querySelectorAll("input.form-field");
   vgsForm.submit(
-    "/post", // change in VGS Dashboard -> Routes -> Manage (inbound route) -> Filters -> Conditions
+    "/", // change in VGS Dashboard -> Routes -> Manage (inbound route) -> Filters -> Conditions
     {
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded"
       },
       method: "POST",
+      data: {
+        "form-name": "collect-form",
+        "field": document.querySelector("input#field").value
+      },
+      serialization: "formData"
     },
-    (status, { json }) => {
-      fields.forEach(field => {
-        json[field.name || field.id] = field.value
-      })
-      handleSubmit(json)
+    (status, state) => {
+      console.log('state', state)
     },
     function(errors) {
       console.error(errors);
